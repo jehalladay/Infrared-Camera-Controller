4018dea991b4b1cee36cff2d205a65ffa668f49a
Alexantoun <oz.antoun@gmail.com>
2022-09-18 18:51:30 -0600

    Re-commiting

---
+++ tests/genImageSeaborn.py
@@ -0,0 +1,19 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pylab as plt
+import pandas as pd
+import statistics
+
+height = 24
+width = 32
+pixel_columns = [f'pixel_{i}' for i in range(width * height)]
+df = pd.read_csv("readcsv.csv")
+
+row = df.iloc[0] # grab next row
+pixels = np.array(row).reshape(height, width)
+min,max = np.min(df.values), np.max(df.values)
+std_deviation = statistics.pstdev(row)
+mean = sum(row) / len(row)
+print(f"The mean reading is {mean} and standard deviation is {std_deviation}")
+ax = sns.heatmap(pixels, vmin = (mean-(2*std_deviation)), vmax =max, center = 0, linewidth=0.5) #Applying range rule of thumb from stat200
+plt.show()
