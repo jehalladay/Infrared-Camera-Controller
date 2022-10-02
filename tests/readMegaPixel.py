eb9c8fe178950e31a849a925f1f2d5d6d4ffdcf8
James Halladay <54321627+jehalladay@users.noreply.github.com>
2022-10-01 18:20:51 -0600

    added code to save a single frame to csv for analysis

--- tests/readMegaPixel.py
+++ tests/readMegaPixel.py
@@ -1,6 +1,11 @@
-import cv2top
-
 import time
+import pandas as np
+
+import cv2
+
+
+
+flag: bool = True
 
 cap = cv2.VideoCapture(0) #Specifies first camera attached to RBPi. This needs fixing, How are the numbers assigned?
 if not cap.isOpened():
@@ -10,5 +15,15 @@ for i in range(5):
     ret, frame = cap.read() #Capture a frame
     out = cv2.imwrite(f"capture_{i}.jpg", frame)
     time.sleep(3)
+
+    if flag:
+        # save frame (a np array) to csv
+
+        flag = False
+        frame = frame.reshape(-1)
+        frame = [str(x) for x in frame]
+        pd.DataFrame(frame).to_csv("frame.csv", index = False, header = False)
+
+
 
 # Used libraries in guide: https://littlebirdelectronics.com.au/guides/165/set-up-opencv-on-raspberry-pi-4
