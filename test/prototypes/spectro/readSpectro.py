e5db1961cccf9d6a1547f65463451576b5e7abd7
Alexantoun <oz.antoun@gmail.com>
2022-10-23 20:40:52 -0600

    Reading spectrometer

--- test/prototypes/spectro/readSpectro.py
+++ test/prototypes/spectro/readSpectro.py
@@ -1,14 +1,41 @@
-#! /usr/bin/python3
-
-import RPi.GPIO as GPIO  
 import time
-print("Running power output tests")
-pinNumber = 29
-GPIO.setmode(GPIO.BOARD) #Sets the type board type of numbers
-GPIO.setup(pinNumber, GPIO.OUT)
-counter = 0
-GPIO.output(pinNumber, 1) #set output to high --> ON
-while(1):
-    GPIO.output(pinNumber, counter) #set output to high --> ON
-    time.sleep(1)
-    counter = (counter + 1)%2
+import board
+from adafruit_as726x import AS726x_I2C
+
+
+max_val = 16000
+max_graph = 500
+
+def graph_map(x):
+    return min(int(x*max_graph/ max_val), max_graph)
+
+def console_write_graphically():
+    print("\n")
+    print("V: " + "="*graph_map((spectro.violet)))
+    print("B: " + "="*graph_map((spectro.blue)))
+    print("R: " + "="*graph_map((spectro.red)))
+    print("Y: " + "="*graph_map((spectro.yellow)))
+    print("G: " + "="*graph_map((spectro.green)))
+
+def console_write_numerically():
+    print("\n")
+    print("V: " + str((spectro.raw_violet)))
+    print("B: " + str((spectro.raw_blue)))
+    print("R: " + str((spectro.raw_red)))
+    print("Y: " + str((spectro.raw_yellow)))
+    print("G: " + str((spectro.raw_green)))
+
+
+i2c = board.I2C() #activates the RBPi default SCL and SDA pins
+spectro = AS726x_I2C(i2c, 0x49) #Spectrometer address at 0x49
+
+spectro.conversion_mode = spectro.MODE_2 #Continuously grab values of all colors
+                                        #Mode 1 is for Green, yellow orange and red only
+                                        #Mode 0 is for Violet, Blue, Green and Yello only
+
+while(True):
+    time.sleep(0.5)
+    console_write_numerically()
+
+
+
