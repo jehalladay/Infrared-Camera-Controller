395bcdac349d48472bc8900ff6a36cc5d80da278
James Halladay <54321627+jehalladay@users.noreply.github.com>
2022-10-29 20:05:49 -0600

    added path validation and creation to cvs_handling

--- driver/utils/csv_handling.py
+++ driver/utils/csv_handling.py
@@ -2,6 +2,10 @@
     File contains utility functions that are used by other files
 '''
 
+from pathlib import (
+    Path
+)
+
 from .types import (
     Frame,
     Metadata,
@@ -14,6 +18,11 @@ def create_csv(file_name: str, columns: list = [], verbose: bool = False):
         If it does not exist, it will create the file
         If it does exist, it will do nothing
     '''
+
+    if not Path(file_name).exists():
+        if verbose:
+            print(f"Creating {Path(file_name).parent} directory(s)")
+        Path(file_name).parent.mkdir(parents=True, exist_ok=True)
 
     try:
         with open(file_name, 'r') as f:
